ad4ad17ff1b95dc59eb69c751bbb3114a947dcb5
Juan Pablo Osorio Restrepo <popeosorio@gmail.com>
2014-06-06 09:47:35 -0500

    List
    List images

---
+++ viewImg.php
@@ -0,0 +1,14 @@
+<?php
+
+Include ("model.php");
+?>
+
+<a href="uploadImg.php">Subir</a> - <a href="viewImg.php">Ver</a> - <a href="search.php">Buscar</a> - <a href="login.php">Login</a>
+
+<?php
+
+data_grid_view();
+?>
+
+
+
